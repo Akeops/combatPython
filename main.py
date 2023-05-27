97088c1a31ccdcbc0300f31adbf211d9ea3117ed
Ando_lalanne <lalanne.andoni1@gmail.com>
2023-05-27 15:19:47 +0200

    j'ai créé le personnage 1 et 2

--- main.py
+++ main.py
@@ -15,5 +15,20 @@ if __name__ == '__main__':
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
-class Pythatchu :
-    self
+class Personnage :
+    def __init__(self, nom, pv):
+        self.nom = nom
+        self.pv = pv
+
+
+
+personnage1 = input("Entrez le nom du premier joueur").capitalize()
+pv1 = input("Entre le nombre de pv du premier joueur").capitalize()
+
+personnage2 = input("Entrez le nom du deuxième joueur").capitalize()
+pv2 = input("Entre le nombre de pv du deuxième joueur").capitalize()
+
+afficher = personnage1 + " (" + str(pv1) + "pv) affronte " + personnage2 + " (" + str(pv2) + "pv)"
+print("+" * (len(afficher)+ 4) )
+print(afficher)
+print("+" * (len(afficher)+ 4) )
