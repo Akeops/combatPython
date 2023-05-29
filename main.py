96d188feb57e38757c6e659658589e537a6f10cc
Ando_lalanne <lalanne.andoni1@gmail.com>
2023-05-29 15:35:35 +0200

    J'ai ajouté le tour du personnage2 et j'ai allégé le code au niveau des if

--- main.py
+++ main.py
@@ -40,71 +40,88 @@ print("+" * (len(msg1)+ 4))
 print()
 
 # Tour 1 - personnage1 attaque personnage2 #
-att1 = int(input(personnage1 + ", quel attaque voulez-vous utiliser ?\n 1. Charge (-20 PV)\n 2. Tonnerre (-40 PV)"))
-msg1 = personnage1 + " attaque Charge sur " + personnage2 + " et celui-ci perd 20 PV"
-
-msg2 = personnage1 + " attaque Tonnerre sur " + personnage2 + " et celui-ci perd 40 PV"
+menu = ''', quelle attaque voulez-vous utiliser ?
+1. Charge (-20 PV)
+2. Tonnerre (-40 PV)'''
 
 print()
+print(personnage1, menu)
+att1 = input('> ').lower()
+print()
 
-if att1 == 1:
-    pv2 -= 20
-    pvRestant2 = personnage2 + " a maintenant " + str(pv2) + " PV !"
-    max_size = max(len(msg1), len(msg2), len(pvRestant2))
-    pvRestant2 += ' ' * (max_size - len(pvRestant2))
-    msg1 += ' ' * (max_size - len(msg1))
-    msg2 += ' ' * (max_size - len(msg2))
-    print("+" * (max_size + 4))
-    print("+", msg1, "+")
-    print("+", pvRestant2, "+")
-    print("+" * (max_size + 4))
-
-elif att1 == 2:
-    pv2 -= 40
-    pvRestant2 = personnage2 + " a maintenant " + str(pv2) + " !"
-    max_size = max(len(msg1), len(msg2), len(pvRestant2))
-    pvRestant2 += ' ' * (max_size - len(pvRestant2))
-    msg1 += ' ' * (max_size - len(msg1))
-    msg2 += ' ' * (max_size - len(msg2))
-    print("+" * (max_size + 4))
-    print("+", msg2, "+")
-    print("+", pvRestant2, "+")
-    print("+" * (max_size + 4))
+if att1 == "1" or att1 == "charge":
+    damages = 20
+    attack = "charge"
+elif att1 == "2" or att1 == "tonnerre":
+    damages = 40
+    attack = "tonnerre"
 else:
+    damages = 0
+    attack = ""
+    print("Vous n'avez pas choisi d'attaque. Vous passez donc le tour.")
+
+
+pv2 -= damages
+msg1 = personnage1 + " attaque " + attack +  " sur " + personnage2 + " qui perd " + str(damages) + " PV."
+msg2 = personnage2 + " a maintenant " + str(pv2) + " !"
+max_size = max(len(msg1), len(msg2))
+msg1 += ' ' * (max_size - len(msg1))
+msg2 += ' ' * (max_size - len(msg2))
+print("+" * (max_size + 4))
+print("+", msg1, "+")
+print("+", msg2, "+")
+print("+" * (max_size + 4))
+
+# Tour 1 = personnage2 attaque personnage1 #
+menu = ''', quelle attaque voulez-vous utiliser ?
+1. Tacle (-15 PV)
+2. Seisme (-50 PV)'''
+
+print()
+print(personnage2, menu)
+att2 = input('> ').lower()
+print()
+
+if att2 == "1" or att2 == "tacle":
+    damages = 15
+    attack = "tacle"
+elif att2 == "2" or att2 == "seisme":
+    damages = 50
+    attack = "seisme"
+else:
+    damages = 0
+    attack = ""
     print("Vous n'avez pas choisi d'attaque. Vous passez donc le tour.")
 
 print()
 
-# Tour 1 = personnage2 attaque personnage1 #
-att2 = int(input(personnage2 + " combien de dégats infligez-vous à " + personnage1 + " ?"))
-pv1 -= att2
-msg1 = personnage2 + " attaque " + personnage1 + " qui perd " + str(att2)
-msg2 = personnage1 + " a maintenant " + str(pv1) + " PV"
-max_size = (max(len(msg1), len(msg2)))
+pv1 -= damages
+msg1 = personnage2 + " attaque " + attack +  " sur " + personnage1 + " qui perd " + str(damages) + " PV."
+msg2 = personnage1 + " a maintenant " + str(pv1) + " PV !"
+max_size = max(len(msg1), len(msg2))
 msg1 += ' ' * (max_size - len(msg1))
 msg2 += ' ' * (max_size - len(msg2))
-
-print()
-
-# Affichage de l'attaque #
-print("+" * (max_size + 4))
-print("+", msg1,  "+")
-print("+",  msg2,  "+")
-print("+" * (max_size + 4))
-
-print()
-
-# Fin du combat #
-msg1 = "Fin du combat"
-msg2 = personnage1 + " a " + str(pv1) + " PV"
-msg3 = personnage2 + " a " + str(pv2) + " PV"
-max_size = max(len(msg1), len(msg2), len(msg3))
-msg1 += ' ' * (max_size - len(msg1))
-msg2 += ' ' * (max_size - len(msg2))
-msg3 += ' ' * (max_size - len(msg3))
-
 print("+" * (max_size + 4))
 print("+", msg1, "+")
 print("+", msg2, "+")
-print("+", msg3, "+")
-print("+" * (max_size+4))
+print("+" * (max_size + 4))
+
+if pv1 == pv2:
+    msg1 = personnage1 + " et " + personnage2 + "ont le même nombre de PV."
+    msg2 = "Il y a donc égalité !"
+if pv1 > pv2:
+    msg1 = personnage1 + " " + str(pv1) +  " PV, a plus de PV que " + personnage2 + " " + str(pv2) + " PV."
+    msg2 = personnage1 + " est le vainqueur du combat !"
+elif pv2 > pv1:
+    msg1 = personnage2 + " " + str(pv2) + " PV, a plus de PV que " + personnage1 + " " + str(pv1) + " PV."
+    msg2 = personnage2 + " est le vainqueur du combat !"
+
+print()
+
+max_size = max(len(msg1), len(msg2))
+msg1 += ' ' * (max_size - len(msg1))
+msg2 += ' ' * (max_size - len(msg2))
+print("+" * (max_size + 4))
+print("+", msg1, "+")
+print("+", msg2, "+")
+print("+" * (max_size + 4))
